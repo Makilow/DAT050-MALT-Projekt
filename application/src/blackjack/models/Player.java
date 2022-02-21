58c405c44a3a0230be5d4b28b9188a4d67b4f05c
Pyyret <98593681+Pyyret@users.noreply.github.com>
2022-02-21 11:41:02 +0100

    Update Player.java

--- application/src/blackjack/models/Player.java
+++ application/src/blackjack/models/Player.java
@@ -3,20 +3,25 @@ package blackjack.models;
 /**
  * Player class representing each player and stats
  * @author Tor Falkenberg
- * @Version 1.2
+ * @Version 1.3
  */
 public class Player {
 
+    //Variables
     private String name;
-    private int money;
+    private double money;
 
-    public Player(String name, int money) {
+    //Constructors
+    public Player() {
+        money = 1000;
+    }
+    public Player(String name, double money) {
         this.name = name;
         this.money = money;
     }
 
-    public int getBalance() { return money; }
-    public void changeBalance(int change) { this.money += change; }
+    public double getBalance() { return money; }
+    public void changeBalance(double change) { this.money += change; }
     public String getName() { return name; }
 
 }
