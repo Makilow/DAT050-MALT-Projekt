f30c124cfe957c72bf4f03bd4121870f05d1ffc6
Pyyret <98593681+Pyyret@users.noreply.github.com>
2022-02-21 19:50:28 +0100

    Add files via upload

--- application/src/blackjack/views/GamePanel.java
+++ application/src/blackjack/views/GamePanel.java
@@ -1,12 +1,17 @@
 package blackjack.views;
 
 import blackjack.controllers.GameController;
-import blackjack.models.Dealer;
-import blackjack.models.MainModel;
+import blackjack.models.*;
 import blackjack.Observer;
-import blackjack.models.Player;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * GamePanel, the panel that showcase the game
@@ -14,15 +19,69 @@ import javax.swing.*;
  */
 public class GamePanel extends JPanel implements Observer<MainModel> {
 
-    public GamePanel(GameController gameController) {
-        dealer = new Dealer();
+    private JPanel panel;
+    private JLabel card1;
+    private JLabel card2;
+    private JLabel card3;
+    private JLabel card4;
+    private JLabel dealer2;
+    private JLabel dealer1;
+    private JButton deal;
 
-        //Test Code
-        for (int i = 0; i < 3; i++) {
-            dealer.addSeat(new Player());
+    private CardDeck deck = new CardDeck();
+
+
+    public ImageIcon cardImage (Card card) {
+        BufferedImage bimage = null;
+        String filename = "src/icons/cards/" + card.getRank().toString() + "_of_" + card.getSuit().toString() + ".png";
+        try {
+            bimage = ImageIO.read(new File(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        Image image = bimage.getScaledInstance(100, 150, Image.SCALE_SMOOTH);
+        return new ImageIcon(image);
+    }
 
-        dealer.newRound();
+    public void deal() {
+        card1.setIcon(cardImage(deck.next()));
+        card2.setIcon(cardImage(deck.next()));
+        card3.setIcon(cardImage(deck.next()));
+        card4.setIcon(cardImage(deck.next()));
+
+        dealer1.setIcon(cardImage(deck.next()));
+        dealer2.setIcon(cardImage(deck.next()));
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public class Hitlistener implements ActionListener {
+
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if (e.getActionCommand() == "hit") {
+                deal();
+            }
+        }
+    }
+
+    public GamePanel(GameController gameController) {
+
+        Hitlistener listener = new Hitlistener();
+
+        setLayout(new GridLayout(1,0));
+        add(panel);
+
+        deal.addActionListener(listener);
+        deal.setActionCommand("hit");
+
+        deal();
+
+
 
     }
 
