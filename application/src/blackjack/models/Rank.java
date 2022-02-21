24e0176064efae2fa217ac03bf0455f71cec0013
PieFreak <75093274+PieFreak@users.noreply.github.com>
2022-02-21 17:55:22 +0100

    Update Rank.java

--- application/src/blackjack/models/Rank.java
+++ application/src/blackjack/models/Rank.java
@@ -4,5 +4,23 @@ package blackjack.models;
  * @author Lukas Wigren
  */
 public enum Rank {
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    ACE("ace"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("jack"),
+    QUEEN("queen"),
+    KING("king");
+
+    private final String name;
+    Rank(String name) { this.name = name;}
+
+    @Override
+    public String toString() {return name;}
 }
