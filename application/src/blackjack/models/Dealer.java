30ef97696a34f570b82ad186b332505c81f40bde
Pyyret <98593681+Pyyret@users.noreply.github.com>
2022-02-21 11:36:05 +0100

    Update Dealer.java
    Förslag på ändringar

--- application/src/blackjack/models/Dealer.java
+++ application/src/blackjack/models/Dealer.java
@@ -1,38 +1,54 @@
+package blackjack.models;
+
 import blackjack.models.CardDeck;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Dealer class containing responsible for table.
+ * @author Tomas Alander (med ändringar av Tor Falkenberg)
+ * @Version 1.0
+ */
+
 public class Dealer {
+
     private int reSuffel = 50;
     private CardDeck cardDeck;
-    private Dealer dealer;
-   // private Hand playerHand = new playerHand();  //Radera!
-    public Dealer() {}
+    private Hand dealerHand = new Hand();
     LinkedList<Hand> playerHands = new LinkedList<>();
+
+    public Dealer() {}
+
     public void startGame() {
-        dealer = new Dealer();
         cardDeck = new CardDeck(7);
     }
-    //Delar ut två kort till alla spelare som är med i handen
-    public void newRound(LinkedList<Hand>playerHands){
+
+    public void addSeat(Player player) {
+        playerHands.add(new Hand(player));
+    }
+
+    public List getSeats() { return playerHands; }
+
+    //Delar ut två kort till alla spelare som är med i rundan
+    public void newRound(){
         // Ligger dealerns hand i listan?
         for(int i=0; i<2; i++){                   //Dela all spelare 2 kort
             for(int j=0; j<playerHands.size(); j++){
                 playerDealOne(playerHands.get(j));
             }
             if(i==0){
-                dealerHand.add(cardDeck.next());
+                dealerHand.addCard(cardDeck.next());
             }else if(i==1){
                 //Ett kort upp och ner?
             }
         }
-        if(dealer.handValu<10){  //Betala alla spelare som fått blackjac
+        if(dealerHand.getValue()<10){  //Betala alla spelare som fått blackjac
             for(int i=0; i<playerHands.size(); i++) {
-                 if (blackJack(playerHands.get(i))) {
-                      playerHands.get(i).payout((playerHands.get(i).showBet()) * 2.5);
-                      playerHands.get(i).muckCards();
-                 }
+                if (blackJack(playerHands.get(i))) {
+                    playerHands.get(i).payout(2.5);
+                }
             }
         }
     }
@@ -40,64 +56,66 @@ public class Dealer {
         if (cardDeck.percentageLeft() <= reSuffel) {
             cardDeck = new CardDeck(7);
         }
-        playerHand.add(cardDeck.next());
+        playerHand.addCard(cardDeck.next());
     }
     public void playerHit(Hand playerHand) {
         if (cardDeck.percentageLeft() <= reSuffel) {
             cardDeck = new CardDeck(7);
         }
-            playerHand.add(cardDeck.next());      //Adderar ett kort till spelarens hand
-            if (playerHand.value() > 21) {
-                playerHand.muchCards();
-            }
-            if (playerHand.getValue == 21){
-             //   next playerHand    //Deala nästa hand
-            }
+        playerHand.addCard(cardDeck.next());      //Adderar ett kort till spelarens hand
+        if (playerHand.getValue() > 21) {
+            playerHand.payout(0);
+        }
+        if (playerHand.getValue() == 21){
+            //   next playerHand    //Deala nästa hand
+        }
     }
     public void doubleHand(Hand playerHand){
         if (cardDeck.percentageLeft() < reSuffel){
             cardDeck = new CardDeck(7);
         }
-        playerHand.add(cardDeck.next());      //Adderar ett kort till spelarens hand
-        if(playerHand.value() > 21) {
-            playerHand.muchCards();
+        playerHand.doubleBet();
+        playerHand.addCard(cardDeck.next());      //Adderar ett kort till spelarens hand
+        if(playerHand.getValue() > 21) {
+            playerHand.payout(0);
         }
     }
-    public voic playerStand(Hand playerHand){
+    public void playerStand(Hand playerHand){
         //next playerHand          //Deala nästa hand
     }
     public boolean blackJack(Hand playerHand){
-        if(playerHand.getNumerOfCards==2 && playerHand.getValue()==21) return true;
-        else return false;
+        if( playerHand.getValue()==21) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void dealDealer(Hand dealerHand){
-        while(dealerHand.value<17) {
+        while(dealerHand.getValue()<17) {
             playerDealOne(dealerHand);
         }
-        if(dealerHand.value>21){
-            dealerLoses(LinkedList<Hand>playerHands);
+        if(dealerHand.getValue()>21){
+            dealerLoses();
         }else{
-            endOfRound(LinkedList<Hand>playerHands);
+            endOfRound();
         }
     }
     //Om dealern blir "tjock"
-    public void dealerLoses(LinkedList<Hand>playerHands){
+    public void dealerLoses(){
         for(int i=0; i<playerHands.size(); i++){
-            playerHands.get(i).payout((playerHands.get(i).getBet())*2); //Spelaren vinner, dubbla insatsen
-            playerHands.get(i).muchCards();
+            playerHands.get(i).payout(2); //Spelaren vinner, dubbla insatsen
+
         }
     }
-    public void endOfRound(LinkedList<Hand>playerHands){
+    public void endOfRound(){
         for(int i=0; i<playerHands.size(); i++){
-            if(playerHands.get(i).getValue() > 16 && playerHands.get(i).getValue() == dealerHand.getValue()){
-                playerHands.get(i).payout(playerHands.get(i).getBet()); //Lika, spelaren behåller sin insats
-                playerHands.get(i).muchCards();
-            }else if(playerHands.get(i).getValue()>dealerHand.getValue()){
-                playerHands.get(i).payout((playerHands.get(i).getBet())*2); //Spelaren vinner, dubbla insatsen
-                playerHands.get(i).muchCards();
+            if(playerHands.get(i).getValue() == dealerHand.getValue()){
+                playerHands.get(i).payout(1); //Lika, spelaren behåller sin insats
+            }else if(playerHands.get(i).getValue()>dealerHand.getValue() && playerHands.get(i).getValue()<=21){
+                playerHands.get(i).payout(2); //Spelaren vinner, dubbla insatsen
             }else{
-                playerHands.get(i).muchCards();      //Spelaren förlorar =(
+                playerHands.get(i).payout(0);      //Spelaren förlorar =(
             }
         }
     }
