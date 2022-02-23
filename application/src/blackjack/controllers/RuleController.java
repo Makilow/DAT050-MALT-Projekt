20381a0277d095497cf75daa61e551fb3660660e
Mark Villarosa <98593614+Makilow@users.noreply.github.com>
2022-02-23 01:11:33 +0100

    Add files via upload

--- application/src/blackjack/controllers/RuleController.java
+++ application/src/blackjack/controllers/RuleController.java
@@ -1,22 +1,30 @@
 package blackjack.controllers;
 
 import blackjack.models.MainModel;
+import blackjack.views.State;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
  * RuleController, a controller for rulePanel
- * @author  Lukas Wigren
+ *
+ * @author Lukas Wigren
  */
 public class RuleController implements ActionListener {
     MainModel mainModel;
+
     public RuleController(MainModel mainModel) {
         this.mainModel = mainModel;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if ("MENU".equals(e.getActionCommand())) {
+            mainModel.setState(State.MENU);
+        } else {
+            throw new IllegalStateException("Error:" + e.getActionCommand());
+        }
     }
 }
+
