9f108bda2809193dbe8485f6bfaf4b2e969162d2
tomasal5817 <57443735+tomasal5817@users.noreply.github.com>
2022-02-23 12:19:15 +0100

    Update Dealer.java

--- application/src/blackjack/models/Dealer.java
+++ application/src/blackjack/models/Dealer.java
@@ -32,20 +32,21 @@ public class Dealer {
         return playerHands;
     }
 
-    //Delar ut två kort till alla spelare som är med i rundan
+    /**
+     * Deal all players two cards, dealer gets 2 cards where the last one is upside down.
+     * if player gets blackjack and dealer hand value < 10, playerHand call function payout.
+     */
     public void newRound() {
-        // Ligger dealerns hand i listan?
-        for (int i = 0; i < 2; i++) {                   //Dela all spelare 2 kort
+        for (int i = 0; i < 2; i++) {
             for (int j = 0; j < playerHands.size(); j++) {
                 playerDealOne(playerHands.get(j));
             }
             if (i == 0) {
                 dealerHand.addCard(cardDeck.next());
             } else if (i == 1) {
-                //Ett kort upp och ner?
             }
         }
-        if (dealerHand.getValue() < 10) {  //Betala alla spelare som fått blackjac
+        if (dealerHand.getValue() < 10) {
             for (int i = 0; i < playerHands.size(); i++) {
                 if (blackJack(playerHands.get(i))) {
                     playerHands.get(i).payout(2.5);
@@ -54,18 +55,29 @@ public class Dealer {
         }
     }
 
-    public void playerDealOne(Hand playerHand) {    //För newRound och dealern
+    /**
+     * Deal next card from cardDeck to player/dealerHand
+     * @param playerHand
+     */
+
+    public void playerDealOne(Hand playerHand) {
         if (cardDeck.percentageLeft() <= reShuffle) {
             cardDeck = new CardDeck(7);
         }
         playerHand.addCard(cardDeck.next());
     }
 
+    /**
+     * Deal playerHand one card, if playerHandValue > 21 "muck" playerHand
+     * if player handValue == 21 deal next player or dealer.
+     * @param playerHand
+     */
+
     public void playerHit(Hand playerHand) {
         if (cardDeck.percentageLeft() <= reShuffle) {
             cardDeck = new CardDeck(7);
         }
-        playerHand.addCard(cardDeck.next());      //Adderar ett kort till spelarens hand
+        playerHand.addCard(cardDeck.next());
         if (playerHand.getValue() > 21) {
             playerHand.payout(0);
         }
@@ -74,79 +86,121 @@ public class Dealer {
         }
     }
 
+    /**
+     * If player handValu [9,11] and player doubles, call doubleBet and deal playerHand one card.
+     * @param playerHand
+     */
     public void doubleHand(Hand playerHand) {
         if (cardDeck.percentageLeft() < reShuffle) {
             cardDeck = new CardDeck(7);
         }
         playerHand.doubleBet();
-        playerHand.addCard(cardDeck.next());      //Adderar ett kort till spelarens hand
+        playerHand.addCard(cardDeck.next());
+      /*
         if (playerHand.getValue() > 21) {
             playerHand.payout(0);
         }
+      */
     }
-
+    /**
+     * Deal next player or call dealDealer
+     * @param playerHand
+     */
     public void playerStand(Hand playerHand) {
-        //next playerHand          //Deala nästa hand
+        /*
+        if(playerHands.hasNext()){}
+        else dealDealer;
+         */
     }
 
+    /**
+     * if playerHand value == 21 and playerHand got two cards the player has blackjack!
+     * @param playerHand
+     * @return true if player or dealer has blackjack
+     */
     public boolean blackJack(Hand playerHand) {
-        if (playerHand.getValue() == 21 && playerHand.getCards().size()==2) {
+        if (playerHand.getValue() == 21 && playerHand.getCards().size() == 2) {
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * dealer draw if handValu < 17 ,dealer stay if handValue [17,21]
+     * if dealer gets blackjack call dealerBlackJack()
+     * if dealer "busts" call dealerLoses
+     * if dealer handValue [17,21] call endOfRound
+     * @param dealerHand
+     */
     public void dealDealer(Hand dealerHand) {
         while (dealerHand.getValue() < 17) {
             playerDealOne(dealerHand);
         }
         if (dealerHand.getValue() > 21) {
             dealerLoses();
-        } else if(blackJack(dealerHand)){
+        } else if (blackJack(dealerHand)) {
             dealerBlackJack();
-        }
-        else {
+        } else {
             endOfRound();
         }
     }
 
-    //Om dealern blir "tjock"
+    /**
+     * dealer got "busted", pay all players left in this hand and "muck" their hands
+     */
     public void dealerLoses() {
         for (int i = 0; i < playerHands.size(); i++) {
-            if(blackJack(playerHands.get(i))){
+            if (blackJack(playerHands.get(i))) {
                 playerHands.get(i).payout(2.5);
-            }else playerHands.get(i).payout(2); //Spelaren vinner, dubbla insatsen
+            } else playerHands.get(i).payout(2);
         }
         dealerHand = new Hand();
     }
+    /**
+     * if player got blackjack it is a "draw/push" else player loses their bets.
+     * check all hands fore insurance.
+     * "Muck" hand after payout
+     */
     public void dealerBlackJack() {
         for (int i = 0; i < playerHands.size(); i++) {
             if (blackJack(playerHands.get(i))) {
-                playerHands.get(i).payout(1); //Lika, spelaren behåller sin insats
+                playerHands.get(i).payout(1); 
             } else {
-                playerHands.get(i).payout(0); //Kollar försäkring
+                playerHands.get(i).payout(0); 
             }
         }
         dealerHand = new Hand();
     }
 
-
+    /**
+     * Compare dealer hand against all players hands left in this hand
+     * if dealer handValu == player handValue it is a "draw/push", player keeps their bets
+     * if playre handValu > dealer handVale -> player wins
+     * if playre handValu < dealer handVale -> dealer wins
+     * "Muck" hand after payout
+     */
     public void endOfRound() {
         for (int i = 0; i < playerHands.size(); i++) {
-            if(blackJack(playerHands.get(i))){
+            if (blackJack(playerHands.get(i))) {
                 playerHands.get(i).payout(2.5);
             } else if (playerHands.get(i).getValue() == dealerHand.getValue()) {
-                playerHands.get(i).payout(1); //Lika, spelaren behåller sin insats
-            } else if (playerHands.get(i).getValue() > dealerHand.getValue() ) {
-                playerHands.get(i).payout(2); //Spelaren vinner, dubbla insatsen
+                playerHands.get(i).payout(1);
+            } else if (playerHands.get(i).getValue() > dealerHand.getValue()) {
+                playerHands.get(i).payout(2);
             } else {
-                playerHands.get(i).payout(0);      //Spelaren förlorar =(
+                playerHands.get(i).payout(0);
             }
         }
         dealerHand = new Hand();
     }
 }
+
+
+
+
+
+
 
 
 
