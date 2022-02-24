71ed48aa234d0b9f74956a5fe5a2ac364ebaba63
PieFreak <75093274+PieFreak@users.noreply.github.com>
2022-02-24 21:58:27 +0100

    Add files via upload

--- application/src/blackjack/views/RulePanel.java
+++ application/src/blackjack/views/RulePanel.java
@@ -1,18 +1,28 @@
 package blackjack.views;
 
+import blackjack.Observer;
 import blackjack.controllers.RuleController;
 import blackjack.models.MainModel;
-import blackjack.Observer;
 
 import javax.swing.*;
+import java.awt.*;
+
 /**
  * RulePanel, the panel that showcase the rules
- * @author Lukas Wigren
+ *
+ * @author Lukas Wigren , Mark Villarosa
  */
 public class RulePanel extends JPanel implements Observer<MainModel> {
 
+    private JTextArea panel;
+    private JPanel panel1;
+    private JButton returnButton;
+
     public RulePanel(RuleController ruleController) {
-        
+        returnButton.setActionCommand(State.MENU.toString());
+        returnButton.addActionListener(ruleController);
+        setLayout(new GridLayout(1, 0));
+        add(panel1);
     }
 
     @Override
