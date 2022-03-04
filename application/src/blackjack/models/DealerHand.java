a0fce33f73a623049ceff7638da84dbf65275841
PieFreak <75093274+PieFreak@users.noreply.github.com>
2022-03-05 00:14:51 +0100

    Update DealerHand.java

--- application/src/blackjack/models/DealerHand.java
+++ application/src/blackjack/models/DealerHand.java
@@ -2,7 +2,10 @@ package blackjack.models;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * DealerHand class
+ * A hand used to store cards of the dealer
+ */
 public class DealerHand extends Hand {
     private final List<Card> cards = new ArrayList<>();
 }
