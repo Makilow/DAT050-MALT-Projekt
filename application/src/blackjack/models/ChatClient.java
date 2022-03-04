99da69b9c36e2c7aa09ea974aa0b9aafb60cab9e
arvmanor <98593701+arvmanor@users.noreply.github.com>
2022-03-04 18:09:29 +0100

    Add files via upload

--- application/src/blackjack/models/ChatClient.java
+++ application/src/blackjack/models/ChatClient.java
@@ -1,4 +1,27 @@
 package blackjack.models;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ChatClient {
+
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    /*  Constructor used to instantiate every property.
+        It takes a socket object for communication with server or chatClientHandler.
+        It also takes a String username to actually represent the client
+     */
+    public ChatClient(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // Send Text-Data
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));   // Read Text-Data
+            this.username = username;
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
 }
