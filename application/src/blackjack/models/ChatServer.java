eaf147194b29abdc34633e6278fba7580617c1d2
arvmanor <98593701+arvmanor@users.noreply.github.com>
2022-03-04 17:20:19 +0100

    Add files via upload

---
+++ application/src/blackjack/models/ChatServer.java
@@ -0,0 +1,55 @@
+package blackjack.models;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+
+    private ServerSocket serverSocket;
+
+    //Constructor creating a serverSocket.
+    public ChatServer(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    //startServer will be responsible for keeping the server running.
+    //server is constantly running until the server socket is closed.
+    public void startServer(){
+
+        try {
+            while(!serverSocket.isClosed()) {
+
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected");
+
+                ChatClientHandler chatClientHandler = new ChatClientHandler(socket);
+
+                Thread thread = new Thread(chatClientHandler);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    // method for handling socket error so that we can avoid nested try-catches
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket = new ServerSocket(1234);
+        ChatServer chatServer = new ChatServer(serverSocket);
+        chatServer.startServer();
+    }
+
+}
