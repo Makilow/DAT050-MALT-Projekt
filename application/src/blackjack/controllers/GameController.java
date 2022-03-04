cc305e3eb3cca293d3aa6c936a3a134ae2a48226
Pyret <98593681+Pyyret@users.noreply.github.com>
2022-03-04 13:14:58 +0100

    Add files via upload

--- application/src/blackjack/controllers/GameController.java
+++ application/src/blackjack/controllers/GameController.java
@@ -20,16 +20,21 @@ public class GameController implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String name;
         switch (e.getActionCommand()) {
+            case "ADD0" -> addPlayer(0);
+            case "ADD1" -> addPlayer(1);
+            case "ADD2" -> addPlayer(2);
+            case "ADD3" -> addPlayer(3);
+            case "ADD4" -> addPlayer(4);
+            case "BET0"-> placeBet(0);
+            case "BET1"-> placeBet(1);
+            case "BET2"-> placeBet(2);
+            case "BET3"-> placeBet(3);
+            case "BET4"-> placeBet(4);
             case "HIT"-> mainModel.playerHit();
             case "STAND"-> mainModel.playerStand();
             case "DOUBLE"-> mainModel.playerDouble();
             case "SPLIT"-> mainModel.playerSplit();
-            case "ADD" -> {
-                name = JOptionPane.showInputDialog(null, "Enter player name:");
-                if (name != null) {
-                    mainModel.addPlayer(name);
-                }
-            }
+
             case "REMOVE" -> {
                 name = JOptionPane.showInputDialog(null, "Enter player name:");
                 if (name != null) {
@@ -39,4 +44,17 @@ public class GameController implements ActionListener {
             default-> System.out.println("HOW DID WE GET HERE?");
         }
     }
+
+    private void addPlayer (int seat) {
+        String name = JOptionPane.showInputDialog(null, "Enter player name:");
+        if (name != null) {
+            mainModel.addPlayer(name, seat);
+        }
+    }
+
+    private void placeBet (int seat) {
+        int bet = Integer.parseInt(JOptionPane.showInputDialog(null, "How much dineros?"));
+        mainModel.playerBet(seat,bet);
+    }
+
 }
