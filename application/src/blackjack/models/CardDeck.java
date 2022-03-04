0c1f2083d5c4bed2de3a356bee448b970f62ef8e
PieFreak <75093274+PieFreak@users.noreply.github.com>
2022-03-04 18:56:59 +0100

    Update CardDeck.java

--- application/src/blackjack/models/CardDeck.java
+++ application/src/blackjack/models/CardDeck.java
@@ -2,7 +2,7 @@ package blackjack.models;
 
 import java.util.*;
 /**
- * CardDeck class Iterator for the Card Class.
+ * CardDeck class Iterator of the Card Class.
  * @author Lukas Wigren
  */
 public class CardDeck implements Iterator<Card> {
@@ -10,10 +10,18 @@ public class CardDeck implements Iterator<Card> {
     private List<Card> deck = new ArrayList<Card>();
     private final int decks;
     private int index = 0;
+    /**
+     * Constructor for CardDeck
+     * Initiates with one deck
+     */
     public CardDeck() {
         this.decks = 1;
         generateCards();
     }
+    /**
+     * Constructor for CardDeck
+     * @param decks number of decks created when initiated
+     */
     public CardDeck(int decks) {
         this.decks = decks;
         generateCards();
@@ -25,14 +33,24 @@ public class CardDeck implements Iterator<Card> {
                     deck.add(new Card(s, r));
         Collections.shuffle(deck);  // Shuffle cards
     }
-    public int percentageLeft() { return 100*(DECK_SIZE*decks-index)/(DECK_SIZE*decks); }  // returns % left of the deck
+    /**
+     * percentageLeft function gets the percentage left of the deck in whole number 0-100
+     * @return  the percentage left of the deck
+     */
+    public int percentageLeft() { return 100*(DECK_SIZE*decks-index)/(DECK_SIZE*decks); }
+    /**
+     * hasNext function check whether the CardDeck iterator has another card
+     * @return  if there is another card
+     */
     @Override
     public boolean hasNext() { return this.deck.size() > index; }
+    /**
+     * next function gets the next card in the deck
+     * @return  the next card
+     */
     @Override
     public Card next() {
         if (!hasNext()) { throw new NoSuchElementException(); }
         return this.deck.get(index++);
     }
-    @Override
-    public void remove() { throw new UnsupportedOperationException(); }
 }
