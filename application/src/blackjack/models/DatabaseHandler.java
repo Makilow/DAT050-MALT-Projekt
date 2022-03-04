8d7675cef146853d7ecc64ea7c3880587a695556
tomasal5817 <57443735+tomasal5817@users.noreply.github.com>
2022-03-04 12:38:21 +0100

    Update DatabaseHandler.java

--- application/src/blackjack/models/DatabaseHandler.java
+++ application/src/blackjack/models/DatabaseHandler.java
@@ -25,59 +25,57 @@ public class DatabaseHandler {
     private final String kol2 = "credits";
 
     private final String insertQuery = "INSERT INTO Scoreboard VALUES(?,?);";
-    private final String getAll = "SELECT* FROM Scoreboard;";
-    private final String getCredits = "SELECT credits FROM Scoreboard WHERE name = ";
-    private final String addCredits = "UPDATE Scoreboard SET credits = ? WHERE name = ?";
-    private final String deletePlayer = "DELETE FROM Scoreboard WHERE name = ?";
-    private final String findPlayer = "SELECT name FROM Scoreboard WHERE name = ?";
-    private final String sortPlayer = "SELECT* FROM Scoreboard ORDER BY credits DESC";
+    private final String getAllQuery = "SELECT* FROM Scoreboard;";
+    private final String getCreditsQuery = "SELECT credits FROM Scoreboard WHERE name = ";
+    private final String addCreditsQuery = "UPDATE Scoreboard SET credits = ? WHERE name = ?";
+    private final String deletePlayerQuery = "DELETE FROM Scoreboard WHERE name = ?";
+    private final String findPlayerQuery = "SELECT name FROM Scoreboard WHERE name = ?";
+    private final String sortPlayerQuery = "SELECT* FROM Scoreboard ORDER BY credits DESC";
 
-    public DatabaseHandler() {
-    }
+    public DatabaseHandler() {}
 
     /**
      * Creates an ArrayList of all players stored in the database
+     *
      * @throws SQLException
      * @throws ClassNotFoundException
      */
     public List<Player> getScoreBoard() throws SQLException, ClassNotFoundException {
         List<Player> playerList = new ArrayList<>();
-        DBMSConnection dbms = new DBMSConnection(url, user, password);
-        Connection con = dbms.connect();
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(sortPlayer);
-        while (rs.next()) {
-            playerList.add(new Player(rs.getString(1), rs.getInt(2)));
+        try {
+            DBMSConnection dbms = new DBMSConnection(url, user, password);
+            Connection con = dbms.connect();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sortPlayerQuery);
+            while (rs.next()) {
+                playerList.add(new Player(rs.getString(1), rs.getInt(2)));
+            }
+            rs.close();
+            dbms.closeConnection(con, st);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        rs.close();
-        dbms.closeConnection(con, st);
         return playerList;
     }
 
-
     /**
      * returns a players credits
+     *
      * @param player
      * @return int
      * @throws SQLException
      * @throws ClassNotFoundException
      */
+    //=================Redundant function?================
     public int getCredits(String player) throws SQLException, ClassNotFoundException {
         int ret = -1;
         try {
             DBMSConnection dbms = new DBMSConnection(url, user, password);
             Connection con = dbms.connect();
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(getCredits + "'" + player + "'" + ";");
+            ResultSet rs = st.executeQuery(getCreditsQuery + "'" + player + "'" + ";");
             rs.next();                 // Låt stå!
             ret = rs.getInt(1);
-            /*
-            while (rs.next()) {
-                String printName = rs.getString(1);
-                ret = rs.getInt(2);
-                System.out.println(printName);
-            }
-             */
             rs.close();
             dbms.closeConnection(con, st);
         } catch (SQLException | ClassNotFoundException e) {
@@ -88,6 +86,7 @@ public class DatabaseHandler {
 
     /**
      * add credits to a players bankroll, credits > 0
+     *
      * @param player
      * @param credits
      * @throws SQLException
@@ -95,35 +94,30 @@ public class DatabaseHandler {
      */
     public void addCredits(String player, int credits) throws SQLException, ClassNotFoundException {
         int oldCredits;
-        if (credits < 0) {
+        if (credits <= 0) {
             System.out.println("credits must be greater than 0");
             return;
         }
         try {
-            oldCredits = this.getCredits(player);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        try {
             DBMSConnection dbms = new DBMSConnection(url, user, password);
             Connection con = dbms.connect();
-            PreparedStatement ps = con.prepareStatement(addCredits);
+            if(getPlayerName(player,con)){
+            oldCredits = this.getPlayerCredits(player,con);
+            PreparedStatement ps = con.prepareStatement(addCreditsQuery);
             ps.setInt(1, credits + oldCredits);
             ps.setString(2, player);
             ps.execute();
             System.out.println("PreparedStatement Insert  successful. name = " + player + ", credits added = " + credits);
             dbms.closeConnection(con, ps);
+            }else System.out.println("Where are no players named " + player + " in this table");
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-
-
     /**
      * if player is in the database table Scoreboard return true
+     *
      * @param player
      * @return boolean
      * @throws SQLException, ClassNotFoundException
@@ -133,7 +127,7 @@ public class DatabaseHandler {
         try {
             DBMSConnection dbms = new DBMSConnection(url, user, password);
             Connection con = dbms.connect();
-            PreparedStatement ps = con.prepareStatement(findPlayer);
+            PreparedStatement ps = con.prepareStatement(findPlayerQuery);
             ps.setString(1, player);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
@@ -145,8 +139,7 @@ public class DatabaseHandler {
                 dbms.closeConnection(con, ps);
                 ret = true;
             }
-        }
-        catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return ret;
@@ -154,6 +147,7 @@ public class DatabaseHandler {
 
     /**
      * remove player from the database table Scoreboard
+     *
      * @param player
      * @throws SQLException
      * @throws ClassNotFoundException
@@ -163,9 +157,14 @@ public class DatabaseHandler {
         try {
             DBMSConnection dbms = new DBMSConnection(url, user, password);
             Connection con = dbms.connect();
-            PreparedStatement ps = con.prepareStatement(deletePlayer);
+            PreparedStatement ps = con.prepareStatement(deletePlayerQuery);
             ps.setString(1, player);
             ps.execute();
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                System.out.println("Where are no players to delete named " + player + " in this table");
+            }
+            rs.close();
             dbms.closeConnection(con, ps);
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -174,6 +173,7 @@ public class DatabaseHandler {
 
     /**
      * Adds a new player to the database table Scoreboard
+     *
      * @param player
      * @param credits
      * @throws SQLException
@@ -184,20 +184,20 @@ public class DatabaseHandler {
             System.out.println("Credits cannot be negative!");
             return;
         }
-        if (!this.playerName(player)) {     //Lös PRIMARY KEY för databasen!
             try {
                 DBMSConnection dbms = new DBMSConnection(url, user, password);
                 Connection con = dbms.connect();
-                PreparedStatement ps = con.prepareStatement(insertQuery);
-                ps.setString(1, player);
-                ps.setInt(2, credits);
-                ps.execute();
-                System.out.println("PreparedStatement Insert  successful. name = " + player + ", credits = " + credits);
-                dbms.closeConnection(con, ps);
+                if (!this.getPlayerName(player,con)) {
+                    PreparedStatement ps = con.prepareStatement(insertQuery);
+                    ps.setString(1, player);
+                    ps.setInt(2, credits);
+                    ps.execute();
+                    System.out.println("PreparedStatement Insert  successful. name = " + player + ", credits = " + credits);
+                    dbms.closeConnection(con, ps);
+                }else System.out.println("Player " +player+ " are already registered");
             } catch (SQLException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
-        } else System.out.println("This player are already registered");
     }
 
     /**
@@ -211,8 +211,7 @@ public class DatabaseHandler {
             DBMSConnection dbms = new DBMSConnection(url, user, password);
             Connection con = dbms.connect();
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(getAll);
-            //rs.next();             //Behövs ej ty while!?
+            ResultSet rs = st.executeQuery(getAllQuery);
             while (rs.next()) {
                 mapPlayers.put(rs.getString(1), rs.getInt(2));
             }
@@ -225,78 +224,114 @@ public class DatabaseHandler {
     }
 
     /**
+     * Returns a new player if the players name is in the database
      * @param name
      * @throws SQLException
      * @throws ClassNotFoundException
      */
     public Player player(String name) throws SQLException, ClassNotFoundException {
-        if (playerName(name)) {
-            try {
-                DBMSConnection dbms = new DBMSConnection(url, user, password);
-                Connection con = dbms.connect();
+        try {
+            DBMSConnection dbms = new DBMSConnection(url, user, password);
+            Connection con = dbms.connect();
+            if (getPlayerName(name,con)) {
                 Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery(getCredits + "'" + name + "'" + ";");
+                ResultSet rs = st.executeQuery(getCreditsQuery + "'" + name + "'" + ";");
                 rs.next();                 // Låt stå!
                 int credits = rs.getInt(1);
                 rs.close();
                 dbms.closeConnection(con, st);
                 return new Player(name, credits);
-            } catch (SQLException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        } else System.out.println("No player with that name");
+            } else System.out.println("No player with that name");
+            return null;
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     /**
-     * add a player to database or update players credits
+     * if player is not in the database add player to database else update player credit balanse
+     *
      * @param player
      * @throws SQLException
      * @throws ClassNotFoundException
      */
-    public void addPlayerData(Player player)throws SQLException, ClassNotFoundException {
-        if (!playerName(player.getName())) {
-            try{ DBMSConnection dbms = new DBMSConnection(url, user, password);
-                Connection con = dbms.connect();
+    public void addPlayerData(Player player) throws SQLException, ClassNotFoundException {
+
+        try {
+            DBMSConnection dbms = new DBMSConnection(url, user, password);
+            Connection con = dbms.connect();
+            if (!getPlayerName(player.getName(), con)) {
                 PreparedStatement ps = con.prepareStatement(insertQuery);
                 ps.setString(1, player.getName());
-                ps.setInt(2, (int)player.getBalance());
+                ps.setInt(2, (int) player.getBalance());
                 ps.execute();
                 System.out.println("PreparedStatement Insert  successful. name = " + player.getName() + ", credits = " + player.getBalance());
                 dbms.closeConnection(con, ps);
-            } catch (SQLException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-
-        } else {
-            try {
-                DBMSConnection dbms = new DBMSConnection(url, user, password);
-                Connection con = dbms.connect();
-                PreparedStatement ps = con.prepareStatement(addCredits);
-                ps.setInt(1, (int)player.getBalance());
+            } else {
+                PreparedStatement ps = con.prepareStatement(addCreditsQuery);
+                ps.setInt(1, (int) player.getBalance());
                 ps.setString(2, player.getName());
                 ps.execute();
                 System.out.println("PreparedStatement Insert  successful. name = " + player.getName() + ", credits added = " + player.getBalance());
                 dbms.closeConnection(con, ps);
-            } catch (SQLException | ClassNotFoundException e) {
-                e.printStackTrace();
             }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
-/* // för testning
+    private boolean getPlayerName(String player,Connection con) throws SQLException{
+        boolean ret = false;
+        try {
+            PreparedStatement ps = con.prepareStatement(findPlayerQuery);
+            ps.setString(1, player);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                System.out.println("Where are no players named " + player + " in this table");
+                rs.close();
+            } else {
+                rs.close();
+                ret = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    private int getPlayerCredits(String player,Connection con) throws SQLException{
+        int ret = 0;
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(getCreditsQuery + "'" + player + "'" + ";");
+            rs.next();                 // Låt stå!
+            ret = rs.getInt(1);
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+ //================For test purpose========================
+    /*
     public static void main(String[] args) throws Exception {
         DatabaseHandler databaseHandler = new DatabaseHandler();
         HashMap<String, Integer> mapScoreboard = databaseHandler.map();
-              mapScoreboard.forEach((k,v) -> {
-              System.out.println("name: " + k + " credits: " + v);
-              });
-/*
-            Player tomas = new Player("jon",333);
-            try {
-                databaseHandler.addPlayerData(tomas);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-*/
+        mapScoreboard.forEach((k, v) -> {
+            System.out.println("name: " + k + " credits: " + v);
+        });
+        //      databaseHandler.addNewPlayer("Jonas",111);
+        // List<Player>player = databaseHandler.getScoreBoard();
+        //Player jon = new Player("jon",333);
+        //databaseHandler.addPlayerData(jon);
+        //Player Karin = databaseHandler.player("Karin");
+        // databaseHandler.addCredits("Tomas",333);
+        // databaseHandler.addCredits("Karin",123);
+    }
+
+     */
 }
