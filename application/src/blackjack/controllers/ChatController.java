795bc7c1a573fb35552065d0b1db17573839eacb
arvmanor <98593701+arvmanor@users.noreply.github.com>
2022-03-05 01:33:47 +0100

    Add files via upload

--- application/src/blackjack/controllers/ChatController.java
+++ application/src/blackjack/controllers/ChatController.java
@@ -1,28 +1,35 @@
 package blackjack.controllers;
 
+import blackjack.models.ChatClient;
+import blackjack.models.ChatHandler;
+import blackjack.models.ChatServer;
 import blackjack.models.MainModel;
 import blackjack.views.State;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 /**
- * RuleController, a controller for rulePanel
- * @author  Lukas Wigren, Mark
+ * ChatController, a controller for ChatPanel
+ * @author  Arvin Allahbakhsh
  */
 public class ChatController implements ActionListener {
-    MainModel mainModel;
-    public ChatController(MainModel mainModel) {
-        this.mainModel = mainModel;
-    }
+
+    private MainModel mainModel;
+    private ChatClient chatClient;
+    private ChatServer chatServer;
+    private ChatHandler chatHandler;
+
+    public ChatController(MainModel mainModel) {this.mainModel = mainModel;}
 
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
             case "MENU" -> mainModel.setState(State.MENU);
+            case "SEND"-> chatClient.sendMessage();
             default -> {
             }
         }
     }
-
 }
