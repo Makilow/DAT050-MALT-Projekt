619c5cfa3d33969a1eef527552ca958b110903b4
PieFreak <75093274+PieFreak@users.noreply.github.com>
2022-03-07 21:52:36 +0100

    Update PlayerHand.java

--- application/src/blackjack/models/PlayerHand.java
+++ application/src/blackjack/models/PlayerHand.java
@@ -10,59 +10,84 @@ import java.util.List;
  */
 
 public class PlayerHand extends Hand {
-    //Private variables
-    private Player player = null;
+    private Player player;
     private double bet = 0;
-    private boolean insured = false;
     private boolean actionDone = true;
 
-    //Constructors
-    public PlayerHand() {}
+    /**
+     * Constructor for PlayerHand
+     * player is set to null
+     */
+    public PlayerHand() { this.player = null;}
+    /**
+     * Constructor for PlayerHand
+     * @param player    A Player
+     */
     public PlayerHand(Player player) { this.player = player;}
     
-    //Public methods
+    /**
+     * Set function, to replace the player
+     * @param player    the new player
+     */
     public void setPlayer(Player player) {
         this.player = player;
         clearHand();
     }
-    
+    /**
+     * Get function, gets the bet
+     * @return  the bet
+     */
     public double getBet() { return bet; }
+    /**
+     * Get function, gets the player
+     * @return  the player
+     */
     public Player getPlayer () {return player;}
+    /**
+     * removePlayer function, sets the player to null and clears hand
+     */
     public void removePlayer() { this.player = null; clearHand(); }
+    /**
+     * setActionDone function, sets the actionDone
+     * @param actionDone    new bool for actionDone
+     */
     public void setActionDone (boolean actionDone) { this.actionDone = actionDone; }
+    /**
+     * isActionDone gets the actionDone boolean
+     * @return  if actionDone
+     */
     public boolean isActionDone () { return this.actionDone; }
-    
+    /**
+     * clearHand function, clears the hand 
+     * cards, bet and actionDone -> true
+     */
     @Override
     public void clearHand() {
         super.clearHand();
         bet = 0;
-        insured = false;
         actionDone = true;
     }
+    /**
+     * bet function, changes the bet by the value passed through
+     * @param bet   the value to change bet by
+     */
     public void bet(double bet) {
         this.bet += bet;
         player.changeBalance(-bet);
     }
-
+    /**
+     * payout function, pays the Player according to multiplier
+     * 0 -> loses the bet
+     * 1 -> no change
+     * 2 -> 2x the bet
+     * @param multiplier    multiplies the bet
+     */
     public void payout(double multiplier) {
         if (player != null) {
-            if (insured) {
-                if(multiplier == 0) {
-                    multiplier = 1;
-                } else {
-                    bet -= bet/3;
-                }
-            }
             player.changeBalance(bet*multiplier);
             clearHand();
             bet = 0;
-            insured = false;
         }
-    }
-
-    public void insure() {
-        insured = true;
-        bet(bet/2);
     }
 }
 
