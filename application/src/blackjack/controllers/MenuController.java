e80baae34775cacbda7cf9d1c129446bb3a5cdc7
Mark Villarosa <98593614+Makilow@users.noreply.github.com>
2022-03-07 12:10:13 +0100

    Update MenuController.java

--- application/src/blackjack/controllers/MenuController.java
+++ application/src/blackjack/controllers/MenuController.java
@@ -19,25 +19,14 @@ public class MenuController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
-            case "GAME":
-                mainModel.setState(State.GAME);
-                break;
-            case "SETTINGS":
-                mainModel.setState(State.SETTINGS);
-                break;
-            case "RULES":
-                mainModel.setState(State.RULES);
-                break;
-            case "SCOREBOARD":
-                mainModel.setState(State.SCOREBOARD);
-                break;
-            case "CHAT":
-                mainModel.setState(State.CHAT);
-                break;
-            case "EXIT":
-                mainModel.setState(State.EXIT);
-                break;
-            default:
+            case "GAME" -> mainModel.setState(State.GAME);
+            case "SETTINGS" -> mainModel.setState(State.SETTINGS);
+            case "RULES" -> mainModel.setState(State.RULES);
+            case "SCOREBOARD" -> mainModel.setState(State.SCOREBOARD);
+            case "CHAT" -> mainModel.setState(State.CHAT);
+            case "EXIT" -> mainModel.setState(State.EXIT);
+            default -> {
+            }
         }
     }
 }
