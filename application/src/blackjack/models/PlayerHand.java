187eab6d612e3715deb483fe56809a00233d48a9
Pyret <98593681+Pyyret@users.noreply.github.com>
2022-03-07 16:12:46 +0100

    Update PlayerHand.java

--- application/src/blackjack/models/PlayerHand.java
+++ application/src/blackjack/models/PlayerHand.java
@@ -12,60 +12,54 @@ import java.util.List;
 public class PlayerHand extends Hand {
     //Private variables
     private Player player = null;
-    //private final List<Card> cards = new ArrayList<>();
     private double bet = 0;
     private boolean insured = false;
+    private boolean actionDone = true;
 
     //Constructors
     public PlayerHand() {}
     public PlayerHand(Player player) { this.player = player;}
-
-
-    //Functions
+    
+    //Public methods
     public void setPlayer(Player player) {
         this.player = player;
         clearHand();
     }
+    
     public double getBet() { return bet; }
     public Player getPlayer () {return player;}
-    public void removePlayer() { this.player = null; }
-
+    public void removePlayer() { this.player = null; clearHand(); }
+    public void setActionDone (boolean actionDone) { this.actionDone = actionDone; }
+    public boolean isActionDone () { return this.actionDone; }
+    
     @Override
     public void clearHand() {
         super.clearHand();
         bet = 0;
         insured = false;
+        actionDone = true;
     }
-
     public void bet(double bet) {
         this.bet += bet;
         player.changeBalance(-bet);
     }
 
-
-
     public void payout(double multiplier) {
-        if (insured) {
-            if(multiplier == 0) {
-                multiplier = 1;
-            } else {
-                bet -= bet/3;
+        if (player != null) {
+            if (insured) {
+                if(multiplier == 0) {
+                    multiplier = 1;
+                } else {
+                    bet -= bet/3;
+                }
             }
+            player.changeBalance(bet*multiplier);
+            clearHand();
+            bet = 0;
+            insured = false;
         }
-        player.changeBalance(bet*multiplier);
-        super.getCards().clear();
-        bet = 0;
-        insured = false;
     }
 
-    public void doubleBet() { bet(this.bet); }
-    public PlayerHand split() {
-        PlayerHand hand = new PlayerHand(player);
-        hand.bet(bet);
-        hand.addCard(super.getCards().get(1));
-        super.getCards().remove(1);
-        return hand;
-    }
     public void insure() {
         insured = true;
         bet(bet/2);
