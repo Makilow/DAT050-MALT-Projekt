5a79605e9a733ad82a9f51f255cf429f3d0fdf6d
Pyret <98593681+Pyyret@users.noreply.github.com>
2022-03-07 23:27:36 +0100

    Update PlayerHandTest.java

--- application/src/blackjack/models/tests/PlayerHandTest.java
+++ application/src/blackjack/models/tests/PlayerHandTest.java
@@ -1,5 +1,7 @@
 package blackjack.models.tests;
 
+import blackjack.models.Player;
+import blackjack.models.PlayerHand;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,41 +10,67 @@ class PlayerHandTest {
 
     @Test
     void setPlayer() {
+        Player testPlayer = new Player("testPlayer", 1000);
+        PlayerHand pH = new PlayerHand();
+        pH.setPlayer(testPlayer);
+        assertEquals(pH.getPlayer(), testPlayer);
     }
 
     @Test
     void getBet() {
+        PlayerHand pH = new PlayerHand();
+        assertEquals(0, pH.getBet());
+
+        pH.setPlayer(new Player("testPlayer", 1000));
+        pH.bet(200);
+        assertEquals(200, pH.getBet());
+
+        pH.clearHand();
+        pH.bet(-200);
+        assertEquals(0, pH.getBet());
     }
 
     @Test
     void getPlayer() {
+        Player testPlayer = new Player("testPlayer", 1000);
+        PlayerHand pH = new PlayerHand();
+        pH.setPlayer(testPlayer);
+        assertEquals(pH.getPlayer(), testPlayer);
     }
 
     @Test
     void removePlayer() {
+        PlayerHand pH = new PlayerHand(new Player("testPlayer", 1000));
+        pH.removePlayer();
+        assertNull(pH.getPlayer());
     }
 
     @Test
     void clearHand() {
+        PlayerHand pH = new PlayerHand(new Player("testPlayer", 1000));
+        pH.setActionDone(false);
+        pH.bet(200);
+        pH.clearHand();
+        assertTrue(pH.isActionDone());
+        assertEquals(0, pH.getBet());
+        assertTrue(pH.getCards().isEmpty());
     }
 
     @Test
     void bet() {
+        PlayerHand pH = new PlayerHand(new Player("testPlayer", 1000));
+        pH.bet(200);
+        assertEquals(200, pH.getBet());
     }
 
     @Test
     void payout() {
-    }
-
-    @Test
-    void doubleBet() {
-    }
-
-    @Test
-    void split() {
-    }
-
-    @Test
-    void insure() {
+        PlayerHand pH = new PlayerHand(new Player("testPlayer", 1000));
+        pH.bet(200);
+        pH.payout(2);
+        assertTrue(pH.isActionDone());
+        assertEquals(0, pH.getBet());
+        assertTrue(pH.getCards().isEmpty());
+        assertEquals(1200, pH.getPlayer().getBalance());
     }
 }
