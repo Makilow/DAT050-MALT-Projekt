455f25d77e950182e3465061c5d52f91ee0ab3c8
PieFreak <75093274+PieFreak@users.noreply.github.com>
2022-03-07 21:11:29 +0100

    Update MainController.java

--- application/src/blackjack/controllers/MainController.java
+++ application/src/blackjack/controllers/MainController.java
@@ -9,12 +9,27 @@ import java.awt.event.*;
  * MainController, a controller for the program, MainView...
  * @author  Lukas Wigren
  */
-public class MainController {
+public class MainController implements KeyListener {
     private MainModel mainModel;
     private MainView mainView;
 
     public MainController(MainModel mainModel, MainView mainView) {
         this.mainModel = mainModel;
         this.mainView = mainView;
+    }
+        @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case 27-> mainModel.setState(State.MENU); // escape
+            default -> {}
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 }
