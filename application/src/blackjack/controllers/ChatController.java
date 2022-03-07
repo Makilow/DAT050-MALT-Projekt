51d93db28c3abc270acbd81ab223f4d37ab97fd2
PieFreak <75093274+PieFreak@users.noreply.github.com>
2022-03-07 21:25:35 +0100

    Update ChatController.java

--- application/src/blackjack/controllers/ChatController.java
+++ application/src/blackjack/controllers/ChatController.java
@@ -1,15 +1,8 @@
 package blackjack.controllers;
 
-import blackjack.models.ChatClient;
-import blackjack.models.ChatHandler;
-import blackjack.models.ChatServer;
 import blackjack.models.MainModel;
-import blackjack.views.ChatPanel;
 import blackjack.views.State;
 
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.BadLocationException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -18,17 +11,23 @@ import java.awt.event.KeyListener;
 
 /**
  * ChatController, a controller for ChatPanel
- * @author  Arvin Allahbakhsh
+ * @author  Arvin Allahbakhsh, Lukas Wigren
  */
 public class ChatController implements ActionListener, KeyListener {
-
     private MainModel mainModel;
     private String message = "";
-
+    
+    /**
+     * Constructor for ChatController
+     * @param mainModel the MainModel
+     */
     public ChatController(MainModel mainModel) {
         this.mainModel = mainModel;
     }
-
+    /**
+     * Actionlistener for Chatcontroller
+     * @param e ActionEvent
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
@@ -41,17 +40,26 @@ public class ChatController implements ActionListener, KeyListener {
             }
         }
     }
-
+    /**
+     * Keytyped listener, adds char to message
+     * @param e KeyEvent
+     */
     @Override
     public void keyTyped(KeyEvent e) {
         message += e.getKeyChar();
     }
-
+    /**
+     * Key pressed, unused
+     * @param e KeyEvent
+     */
     @Override
     public void keyPressed(KeyEvent e) {
 
     }
-
+    /**
+     * Key released, unused
+     * @param e KeyEvent
+     */
     @Override
     public void keyReleased(KeyEvent e) {
 
